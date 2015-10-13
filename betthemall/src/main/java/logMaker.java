5d83c804ab95456ebd0b95d203dc984d312a3ea3
MrZacky <lukasz.slowik01@gmail.com>
2015-10-13 19:47:32 +0200

    logMaker.java - Dodana możliwość tworzenia logów na heroku i przesyłania ich na serwer vps.

--- betthemall/src/main/java/logMaker.java
+++ betthemall/src/main/java/logMaker.java
@@ -1,5 +1,6 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,9 +9,24 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
 public class logMaker {
 	
 	File file = null;
+	
+	String SFTPHOST = "91.189.37.233";
+	int SFTPPORT = 22;
+	String SFTPUSER = "root";
+	String SFTPPASS = "Wakacje123";
+	String SFTPWORKINGDIR = "/root/betthemall/logs";
+
+	Session session = null;
+	Channel channel = null;
+	ChannelSftp channelSftp = null;
 
     private void logAdder(String msg)
     {	
@@ -23,27 +39,24 @@ public class logMaker {
 		  
 		    //get current date time with Calendar()
 		    Calendar cal = Calendar.getInstance();
-		    String newNameOfFile = "logs/"+dateFormat.format(cal.getTime()).toString()+"_betthemall.log";
 		    
     		if (file==null){
+    			String newNameOfFile = "tmp/logs/"+dateFormat.format(cal.getTime()).toString()+"_betthemall.log";
     			file = new File(newNameOfFile);
     			//System.out.println("File null");
     		}
     		else{
     			long fileSizeInMB = file.length() / 1024 / 1024;
     			if (fileSizeInMB >= 10){
+    				String newNameOfFile = "tmp/logs/"+dateFormat.format(cal.getTime()).toString()+"_betthemall.log";
     				file = new File(newNameOfFile);
     			}
     		}
     		
-    		//if file doesnt exists, then create it
     		if(!file.exists()){
-    			//System.out.println("File not exist");
     			file.createNewFile();
-    			//System.out.println("File created");
-    		}
+			}
     		
-    		//true = append file
     	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
     	    out.println(msg);
     	    out.close();
@@ -51,6 +64,26 @@ public class logMaker {
     	}catch(IOException e){
     		e.printStackTrace();
     	}
+    	
+		try {
+			JSch jsch = new JSch();
+			session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+			session.setPassword(SFTPPASS);
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			channel = session.openChannel("sftp");
+			channel.connect();
+			channelSftp = (ChannelSftp) channel;
+			channelSftp.cd(SFTPWORKINGDIR);
+			//File f = new File("test.txt");
+		
+			channelSftp.put(new FileInputStream(file), file.getName());
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
     }
 
 	public void logAdd (String msg) {
